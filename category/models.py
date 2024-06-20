b67c134d6fd9a4fad66df14e1a547958e9b88c92
Gaitho57 <michaelgaitho10@gmail.com>
2024-06-20 14:02:05 +0300

    [feat]: category model - admin

--- category/models.py
+++ category/models.py
@@ -6,5 +6,12 @@ class Category(models.Model):
   description = models.TextField(blank=True)  # Optional description
   cat_image = models.ImageField(upload_to='photos/categories/', blank=True)
 
+
+
+
+  class Meta:
+  	verbose_name = 'category'
+  	verbose_name_plural = 'categories'
+
   def __str__(self):
     return self.category_name

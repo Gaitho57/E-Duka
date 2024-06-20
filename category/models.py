80be632960ce81a25e81210ec00ad4aaa994e9ae
Gaitho57 <michaelgaitho10@gmail.com>
2024-06-20 13:52:34 +0300

    [feat]: category app - category model

---
+++ category/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Category(models.Model):
+  category_name = models.CharField(max_length=255, unique=True)  # Name of the category
+  slug = models.CharField(max_length=50, unique=True)
+  description = models.TextField(blank=True)  # Optional description
+  cat_image = models.ImageField(upload_to='photos/categories/', blank=True)
+
+  def __str__(self):
+    return self.category_name
